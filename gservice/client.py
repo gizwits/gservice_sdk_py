01ca6d6ab14f0135199a5778a8abab69d06b72b0
samleung <sliang@xtremeprog.com>
2014-11-26 11:51:44 +0800

    update request

--- gservice/client.py
+++ gservice/client.py
@@ -21,6 +21,8 @@ class GServiceClient(APIClient):
         self.headers.update({
                 'X-Gizwits-Application-Id': appid,
                 })
+
+    # ===user
     @auto_token
     def create_user_by_username(self, username, password):
         r = g_users.create_user_by_username(username, password)
@@ -59,6 +61,37 @@ class GServiceClient(APIClient):
     def login_by_phone(self, phone, password):
         return self._login(phone, password)
 
+    # update
+    def update_info(self, username, password):
+        r = g_users.update_info(username, password)
+        return self.send_request(r)
+
+    def update_pwd(self, old_pwd, new_pwd):
+        r = g_users.update_pwd(username, password)
+        return self.send_request(r)
+
+    def update_email(self, email):
+        r = g_users.update_email(email)
+        return self.send_request(r)
+
+    def update_phone_code(self, phone, code):
+        r = g_users.update_phone_code(phone, code)
+        return self.send_request(r)
+
+    def update_phone_pwd_code(self, phone, pwd, code):
+        r = g_users.update_phone_pwd_code(phone, pwd, code)
+        return self.send_request(r)
+
+    # password reset
+    def password_reset(self, email):
+        r = g_users.password_reset(email)
+        return self.send_request(r)
+
+    def password_reset_with(self, phone, code, new_pwd):
+        r = g_users.password_reset_with(phone, code, new_pwd)
+        return self.send_request(r)
+
+    #===codes
     def get_code(self, phone):
         r = g_codes.get_code(phone, password, code)
         return self.send_request(r)
@@ -67,6 +100,37 @@ class GServiceClient(APIClient):
         r = g_codes.verify_code(phone, code)
         return self.send_request(r)
 
+    # === device
+    def retrieve_device_histroy_data(did, start_ts=1349032093,
+                                     end_ts=1349032093, entity=1,
+                                     attr="temp", limit=20,
+                                     skip=0):
+        r = g_device.retrieve_device_histroy_data(did,
+                                                  start_ts,
+                                                  end_ts,
+                                                  entity,
+                                                  attr,
+                                                  limit,
+                                                  skip
+                                                  )
+        return self.send_request(r)
+
+    def retrieve_product_histroy_data(product_key, did=None,
+                                      start_ts=1349032093, end_ts=1349032093,
+                                      entity=1, attr="temp",
+                                      limit=20, skip=0):
+        r = g_device.retrieve_product_histroy_data(product_key,
+                                          did,
+                                          start_ts,
+                                          end_ts,
+                                          entity,
+                                          attr,
+                                          limit,
+                                          skip
+                                          )
+        return self.send_request(r)
+
+    # bound device
     def bind_device(self, devices):
         '''
         :param devices: struct = > [('did', 'passcode'), ...]
@@ -75,6 +139,12 @@ class GServiceClient(APIClient):
         r = g_device.bind_devices(devices)
         return self.send_request(r)
 
+    def unbind_devices(self, devices):
+        '''
+        :param devices: struct = > [('did', 'passcode'), ...]
+        '''
+        r = g_device.unbind_devices(devices)
+        return self.send_request(r)
 
     def control_device(self, did, raw):
         '''
@@ -83,11 +153,30 @@ class GServiceClient(APIClient):
 
         :param raw: struct => [<byte>, <byte>, ...]
         :type raw: list
-
         '''
         r = g_device.remote_control_device(did, raw)
         return self.send_request(r)
 
-
     def get_bind_device(self, limit=20, skip=0):
         return self.send_request(g_device.get_bound_devices(limit, skip))
+
+
+    # device detail
+    def device_detail(self, did):
+        r = g_device.device_detail(did)
+        return self.send_request(r)
+    
+    def query_device(self, product_key, mac):
+        r = g_device.query_device(product_key, mac)
+        return self.send_request(r)
+
+    def remote_control_device(self, did, raw):
+        '''
+        :param did: did
+        :type did: String
+
+        :param raw: struct => [<byte>, <byte>, ...]
+        :type raw: list
+        '''
+        r = g_device.remote_control_device(did, raw)
+        return self.send_request(r)
